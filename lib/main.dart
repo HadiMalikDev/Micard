5b2d3c4e1517fc0b89b89198a5335592640f4ac5
MHadi-Dev <Hadimalik1027@gmail.com>
2021-02-02 15:53:46 +0500

    micard commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,81 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MySecondApp());
+}
+
+class MySecondApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.black,
+            body: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/img.png"),
+                    radius: 55.0,
+                  ),
+                  Text(
+                    "Doge",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38.0,
+                        fontFamily: "Pacifico"),
+                  ),
+                  Text(
+                    "Flutter Developer",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: "Source Sans Pro",
+                        letterSpacing: 5.0),
+                  ),
+                  Container(
+                      width: 200,
+                      child: Divider(
+                        color: Colors.white,
+                        height: 30,
+                      )),
+                  Container(
+                    width: 330,
+                    child: Card(
+                      color: Colors.white,
+                      child: ListTile(
+                        leading:
+                            Icon(Icons.phone, size: 25.0, color: Colors.amber),
+                        title: Text("+92 3335671463",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 20.0,fontFamily: "Source Sans Pro")),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 330,
+                    child: Card(
+                      color: Colors.white,
+                      child: ListTile(
+                        leading:
+                            Icon(Icons.email, size: 25.0, color: Colors.amber),
+                        title: Text("Doge@gmail.com",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 20.0,fontFamily: "Source Sans Pro")),
+                      
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )));
+  }
 }
